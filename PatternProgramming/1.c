ee7cebe82ebbe5626c064b949393934e04bbb5fc
ms-ayrawhsia <85438061+ms-ayrawhsia@users.noreply.github.com>
2021-06-06 16:39:56 +0530

    Create 1.c

---
+++ PatternProgramming/1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int main() {
+    int i,j;  // i:Rows and j:Columns
+     for(i=1;i<=5;i++) {
+      for(j=1;j<=5;j++) {
+        printf(" * ");
+      }
+      printf("\n");
+     }
+}
+
+/*
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
